129a4e15eae34bdf20afe72e88302340cb4178cd
Phan M. Hung <pmhungpro1@gmail.com>
2023-01-09 17:02:54 +0700

    feat: not found page

--- src/page/NotFound.js
+++ src/page/NotFound.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import {useNavigate} from "react-router-dom";
+const Error = () => {
+    const navigate = useNavigate();
+    return (
+        <div
+            className='w-screen h-screen font-[Exo] text-black relative '
+            style={{backgroundImage: "url(/images/404bg.jpg)", backgroundSize: "cover"}}>
+            
+            <div className='mt-[15px] flex items-center justify-center gap-x-[23px] text-[20px] text-light '>
+                <div
+                    className='py-[5px] px-[28px] border border-black rounded-[5px] cursor-pointer hover:bg-white/20 transition-50 '
+                    onClick={() => {
+                        navigate("/");
+                    }}>
+                    GO HOME
+                </div>
+                <div
+                    className='py-[5px] px-[28px] border border-black rounded-[5px] cursor-pointer hover:bg-white/20 transition-50 '
+                    onClick={() => {
+                        navigate(-1);
+                    }}>
+                    GO BACK
+                </div>
+            </div>
+        </div>
+    );
+};
 
-function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
-}
-
-export default NotFound
+export default Error;
